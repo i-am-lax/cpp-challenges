c0b3470a07f0c45d1c856ac2a2c1d40ec764836e
i-am-lax <100245799+i-am-lax@users.noreply.github.com>
2022-12-23 18:54:20 +0000

    add encode fn

--- challenges/soundex/soundex.cpp
+++ challenges/soundex/soundex.cpp
@@ -1,1 +1,40 @@
 #include "soundex.h"
+#include <cctype>
+
+// TO DELETE
+#include <iostream>
+using namespace std;
+
+/* Generates the Soundex encoding corresponding to a given 'surname' and writes
+ * result to 'soundex':
+ - letter at the start of the Soundex code is the first letter of the surname
+ - remaining digits looked up from 'CODES' map (adjacent letters with same code
+ treated as one)
+ - empty slots padded with zeros */
+void encode(const char *surname, char *soundex) {
+    // initialise variables for code lookups
+    char code, prev = '0';
+
+    // pad soundex code with zeros
+    strcpy(soundex, "0000");
+
+    // index for soundex
+    int idx = 0;
+    while (*surname != '\0' && soundex[idx] != '\0') {
+        // add first letter of surname
+        if (idx == 0) {
+            soundex[idx] = toupper(*surname);
+            idx++;
+        }
+        // lookup code and and ensure it is not equal to the previous one
+        else if (CODES.count(*surname)) {
+            code = CODES.at(*surname);
+            if (code != prev) {
+                soundex[idx] = code;
+                prev = code;
+                idx++;
+            }
+        }
+        surname++;
+    }
+}
--- challenges/soundex/soundex.h
+++ challenges/soundex/soundex.h
@@ -1,4 +1,17 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
+#include <map>
+
+// Mapping for digits of the Soundex code
+const std::map<char, char> CODES = {
+    {'b', '1'}, {'f', '1'}, {'p', '1'}, {'v', '1'}, {'c', '2'}, {'g', '2'},
+    {'j', '2'}, {'k', '2'}, {'q', '2'}, {'s', '2'}, {'x', '2'}, {'z', '2'},
+    {'d', '3'}, {'t', '3'}, {'l', '4'}, {'m', '5'}, {'n', '5'}, {'r', '6'},
+};
+
+/* Generates the Soundex encoding corresponding to a given 'surname' and writes
+ * result to 'soundex' */
+void encode(const char *surname, char *soundex);
+
 #endif
